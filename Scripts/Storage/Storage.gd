e5b8e0d8e332bec1f0a6cd0ebbbea13422b3a446
Kirk Walek <lithiumleech@gmail.com>
2023-10-30 22:48:52 -0400

    Add storage version

--- Scripts/Storage/Storage.gd
+++ Scripts/Storage/Storage.gd
@@ -9,6 +9,7 @@ const SAVE_PATH: String = "user://data.bin"
 # Stores the current state of the app.
 static func save_app_data():
 	var file: FileAccess = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	file.store_var(1) # Marks the version of this storage implementation.
 	file.store_var(Main.game_state.start_board)
 	file.store_var(Main.game_state.start_set)
 	file.store_var(Main.game_state.high_score)
@@ -20,9 +21,16 @@ static func save_app_data():
 static func load_app_data():
 	if (FileAccess.file_exists(SAVE_PATH)):
 		var file: FileAccess = FileAccess.open(SAVE_PATH, FileAccess.READ)
-		Main.game_state.start_board = file.get_var()
-		Main.game_state.start_set = file.get_var()
-		Main.game_state.high_score = file.get_var()
-		Main.music_box.set_volume(file.get_var())
-		Main.atlas.current_locale = file.get_var()
-		TranslationServer.set_locale(Main.atlas.current_locale)
+		var version: int = file.get_var()
+		match version:
+			1: load_version_1(file)
+
+
+# Restores data stored by storage version 1.
+static func load_version_1(file: FileAccess):
+	Main.game_state.start_board = file.get_var()
+	Main.game_state.start_set = file.get_var()
+	Main.game_state.high_score = file.get_var()
+	Main.music_box.set_volume(file.get_var())
+	Main.atlas.current_locale = file.get_var()
+	TranslationServer.set_locale(Main.atlas.current_locale)
