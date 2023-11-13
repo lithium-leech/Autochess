97b92233198ca6782c48d19add43e97fe810e06f
Kirk Walek <lithiumleech@gmail.com>
2023-11-13 11:42:40 -0500

    Add script to each piece prefab

---
+++ Scripts/Game/Pieces/Hisha.gd
@@ -0,0 +1,24 @@
+# A chess piece.
+extends Piece
+
+
+# The kind of piece this is.
+func get_kind() -> Kind:
+	return Kind.HISHA
+
+
+# Takes the piece's turn.
+func take_turn():
+	# Assume initially that the piece cannot move.
+	var _path: Array[Vector2i] = [space.coordinates]
+	# Get the possible moves and captures this piece can make.
+	var moves: Array = []
+	var captures: Array = []
+	# Capture a piece if possible.
+	if (captures.size() > 0):
+		_path = captures[randi_range(0, captures.size() - 1)]
+	# Otherwise, move if possible.
+	elif (moves.size() > 0):
+		_path = moves[randi_range(0, moves.size() - 1)]
+	# Move to the new space.
+	start_turn(_path)
