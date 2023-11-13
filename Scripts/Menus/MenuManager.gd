69016e0b62425969e7c7380451c882c276c301fa
Kirk Walek <lithiumleech@gmail.com>
2023-11-12 23:49:04 -0500

    Re-add multi-menus

--- Scripts/Menus/MenuManager.gd
+++ Scripts/Menus/MenuManager.gd
@@ -3,7 +3,7 @@ class_name MenuManager
 
 
 # The active menu.
-var active_menu: Menu
+var active_menus: Array[Menu]
 
 # An additional menu displayed over the active menu.
 var overlay_menu: Menu
@@ -15,10 +15,25 @@ func initialize(menu: Menu):
 	if (overlay_menu != null):
 		overlay_menu.close()
 		overlay_menu = null
-	if (active_menu != null):
+	for active_menu in active_menus:	
 		active_menu.close()
-	active_menu = menu
-	active_menu.open()
+	active_menus = [menu]
+	active_menus[0].open()
+
+
+# Adds a menu to the active menus.
+# 	menu: The menu to make active.
+func add_active_menu(menu: Menu):
+	if (overlay_menu == null):
+		menu.open()
+	active_menus.append(menu)
+
+
+# Removes a menu from the active menus.
+# 	menu: The menu to deactivate.
+func remove_active_menu(menu: Menu):
+	menu.close()
+	active_menus.erase(menu)
 
 
 # Shows a new menu above the active menu.
@@ -27,7 +42,8 @@ func open_overlay(menu: Menu):
 	if (overlay_menu != null):
 		overlay_menu.close()
 	else:
-		active_menu.close()
+		for active_menu in active_menus:
+			active_menu.close()
 	overlay_menu = menu
 	overlay_menu.open()
 
@@ -36,4 +52,5 @@ func open_overlay(menu: Menu):
 func close_overlay():
 	overlay_menu.close()
 	overlay_menu = null
-	active_menu.open()
+	for active_menu in active_menus:
+		active_menu.open()
